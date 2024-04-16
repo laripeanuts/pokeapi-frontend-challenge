9e1d00d665fe8e6174a3b9e6be8072cf57f7ff15
Larissa Rabelo <larissarabelolf@gmail.com>
2024-04-15 21:06:33 -0300

    fix(components): handle correctly the handle for close modal

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -3,12 +3,12 @@ import { Spin } from "antd";
 import { useState } from "react";
 
 import { Pokemon } from "@/@types/pokemon";
-import PokemonModal from "@/components/PokemonModal";
+import { PokemonCardImage } from "@/components/Card/PokemonCardImage";
+import { PokemonModal } from "@/components/PokemonModal";
 import { PokemonTypeImage } from "@/components/PokemonTypeImage";
 import { useGetPokemon } from "@/queries/pokemons";
 import { getColorBySpecies } from "@/utils/theme";
 
-import { PokemonCardImage } from "@/components/Card/PokemonCardImage";
 import * as S from "./styles";
 
 type CardProps = {
@@ -18,7 +18,8 @@ type CardProps = {
 export const Card = ({ pokemonId }: CardProps) => {
   const { isLoading, data: pokemon } = useGetPokemon(pokemonId);
 
-  const [showPokemonDetailsModal, setShowPokemonDetailsModal] = useState(false);
+  const [isPokemonDetailModalOpen, setShowPokemonDetailsModal] =
+    useState(false);
 
   const mainType = pokemon?.types[0].type.name || "default";
   const mainImage = pokemon?.sprites.other["official-artwork"].front_default;
@@ -33,35 +34,40 @@ export const Card = ({ pokemonId }: CardProps) => {
   };
 
   return (
-    <S.Container speciescolor={color} onClick={openModal}>
-      {isLoading ? (
-        <S.LoadingContainer>
-          <Spin indicator={<LoadingOutlined spin />} />
-        </S.LoadingContainer>
-      ) : (
-        <S.CardContent>
-          <S.CardHeader>
-            <span>
-              #<strong>{pokemon?.id}</strong>
-            </span>
-            <S.CardTypes>
-              {pokemon?.types.map((type) => (
-                <PokemonTypeImage key={type.type.name} type={type.type.name} />
-              ))}
-            </S.CardTypes>
-          </S.CardHeader>
-          <PokemonCardImage image={mainImage} name={pokemon?.name} />
-          <S.CardFooter>
-            <p>{pokemon?.name}</p>
-          </S.CardFooter>
-          <PokemonModal
-            pokemon={pokemon as Pokemon}
-            isOpen={showPokemonDetailsModal}
-            setClosed={closeModal}
-            color={color}
-          />
-        </S.CardContent>
-      )}
-    </S.Container>
+    <>
+      <S.Container speciescolor={color} onClick={openModal} key={pokemon?.id}>
+        {isLoading ? (
+          <S.LoadingContainer>
+            <Spin indicator={<LoadingOutlined spin />} />
+          </S.LoadingContainer>
+        ) : (
+          <S.CardContent>
+            <S.CardHeader>
+              <span>
+                #<strong>{pokemon?.id}</strong>
+              </span>
+              <S.CardTypes>
+                {pokemon?.types.map((type) => (
+                  <PokemonTypeImage
+                    key={type.type.name}
+                    type={type.type.name}
+                  />
+                ))}
+              </S.CardTypes>
+            </S.CardHeader>
+            <PokemonCardImage image={mainImage} name={pokemon?.name} />
+            <S.CardFooter>
+              <p>{pokemon?.name}</p>
+            </S.CardFooter>
+          </S.CardContent>
+        )}
+      </S.Container>
+      <PokemonModal
+        pokemon={pokemon as Pokemon}
+        isModalOpened={isPokemonDetailModalOpen}
+        setModalClosed={closeModal}
+        color={color}
+      />
+    </>
   );
 };
